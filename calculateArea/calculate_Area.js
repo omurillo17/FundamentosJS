63eeaee784d6cd1ee3c5bb20f311c6bf0d80b3fc
Oscar Murillo <oscarone2002@gmail.com>
2025-05-27 22:53:09 -0400

    mensaje

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,20 @@
 let length;
 let width;
+let monto1, monto2, monto3;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
-    document.getElementById('result').innerText = 'The area of the rectangle is: ${area}';
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    }
+
+function groceryTracker(){
+    monto1 = parseFloat(document.getElementById('grocery1').value);
+    monto2 = parseFloat(document.getElementById('grocery2').value);
+    monto3 = parseFloat(document.getElementById('grocery3').value);
+
+    let totalMount = monto1 + monto2 + monto3;
+    document.getElementById('result').innerText = `El monto total es: ${totalMount}`;
+}
