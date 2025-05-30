23252b278db2197ef414583627d082e76290d44d
Oscar Murillo <oscarone2002@gmail.com>
2025-05-29 22:34:16 -0400

    En esta actividad aprendi a acceder a crear un arreglo/lista de libros, estos son ingresados en un crud

---
+++ bookSystem/book_system.js
@@ -0,0 +1,54 @@
+let books = [];
+  function addBook() {
+            const bookName = document.getElementById('bookName').value;
+            const authorName = document.getElementById('authorName').value;
+            const bookDescription = document.getElementById('bookDescription').value;
+            const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+            if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+                const book = {
+                    name: bookName,
+                    authorName: authorName,
+                    bookDescription: bookDescription,
+                    pagesNumber: pagesNumber
+                };
+                books.push(book);
+                showbooks();
+                clearInputs();
+            } else {
+                alert('Por favor, complete todos los campos correctamente.');
+            }
+        }
+
+function showbooks() {
+    const booksDiv = books.map((book, index) => `<h1>Número de libro: ${index + 1}</h1>
+        <p><strong>Nombre del libro: </strong>${book.name}</p>
+        <p><strong>Nombre del autor:</strong> ${book.authorName}</p>
+        <p><strong>Descripción del libro:</strong> ${book.bookDescription}</p>
+        <p><strong>Número de páginas:</strong> ${book.pagesNumber} página(s)</p>
+        <button onclick="editbook(${index})">Editar</button>
+        <button onclick="deletebook(${index})">Eliminar</button>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+function editbook(index) {
+    const book = books[index];
+    document.getElementById('bookName').value = book.name;
+    document.getElementById('authorName').value = book.authorName;
+    document.getElementById('bookDescription').value = book.bookDescription;
+    document.getElementById('pagesNumber').value = book.pagesNumber;
+    books.splice(index, 1); // Eliminar entrada antigua
+    showbooks(); // Actualizar lista
+  }
+
+   function clearInputs() {
+            document.getElementByIds('bookName').value = '';
+            document.getElementById('authorName').value = '';
+            document.getElementById('bookDescription').value = '';
+            document.getElementById('pagesNumber').value = '';
+ }
+
+function deletebook(index){
+    books.splice(index, 1);
+    showbooks();
+}
